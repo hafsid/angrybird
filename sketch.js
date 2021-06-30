7485dba7e0701e2349e130c471b7df588ef49d8e
hafsid <50413142+hafsid@users.noreply.github.com>
2021-06-30 16:08:10 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -35,23 +35,5 @@ function setup(){
 function draw(){
     background(0);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
-    box1.display();
-    box2.display();
-    ground.display();
-    pig1.display();
-    log1.display();
-
-    box3.display();
-    box4.display();
-    pig3.display();
-    log3.display();
-
-    box5.display();
-    log4.display();
-    log5.display();
-
-    bird.display();
+   
 }
